1829c15d40b6496784f1b5543b80760366ed9a3d
Thyerry Nunes dos Santos <thyerry.nunes@gmail.com>
2021-12-03 00:52:48 -0300

    organizando melhor os endpoints

--- Backend/SandCastle-BackEnd/SandCastle-BackEnd/Program.cs
+++ Backend/SandCastle-BackEnd/SandCastle-BackEnd/Program.cs
@@ -1,5 +1,6 @@
 using MongoDB.Driver;
 using SandCastle_BackEnd.MongoRelated;
+using System.Net;
 
 // Configurações de Build
 var builder = WebApplication.CreateBuilder(args);
@@ -19,16 +20,32 @@ app.UseSwagger();
 
 // Endpoints
 // Pega todas as Fichas
-app.MapGet("/Fichas", async () => await fichaContext.Fichas.FindAsync(p => true));
+app.MapGet("/Fichas", async () =>
+{
+    var result = await fichaContext.Fichas.FindAsync(p => true);
+    return result.ToList();
+});
 
 // Pega ficha específica
-app.MapGet("/Fichas/{name}", async (string id) => await fichaContext.Fichas.FindAsync(p => p.Id == id));
+app.MapGet("/Fichas/{id}", async (string id) => 
+{ 
+    var result = await fichaContext.Fichas.FindAsync(p => p.Id == id);
+    return result.FirstOrDefault();
+});
 
 // Cria nova Ficha
-app.MapPost("/Fichas", async (Ficha insert) => await fichaContext.Fichas.InsertOneAsync(insert));
+app.MapPost("/Fichas", async (Ficha insert) =>
+{
+    await fichaContext.Fichas.InsertOneAsync(insert);
+    return HttpStatusCode.Created;
+});
 
 // Atualiza uma ficha específica
-app.MapPut("/Fichas/", async (Ficha update) => await fichaContext.Fichas.ReplaceOneAsync(f => f.Id == update.Id, update));
+app.MapPut("/Fichas/", async (Ficha update) =>
+{
+    var result = await fichaContext.Fichas.ReplaceOneAsync(f => f.Id == update.Id, update);
+    return result;
+});
 
 // Deleta uma ficha específica
 app.MapDelete("/Fichas/{id}", (string id) => fichaContext.Fichas.FindOneAndDelete(p => p.Id == id));
